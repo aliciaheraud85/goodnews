3f504a8c0b3663878cd251cce032a46429cea441
aliciaheraud85 <167339080+aliciaheraud85@users.noreply.github.com>
2024-06-26 16:36:06 +0200

    modif header

--- elements/header.php
+++ elements/header.php
@@ -1,24 +1,45 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
+<link rel="stylesheet" href="/goodnews/divers/css/header.css">
+
+<div class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-11  d-flex justify-content-center">
+                <img class="img-fluid" src="/goodnews/divers/images/logoblanc.png" alt="">
+            </div>
+            <div class="col-md-1">
+                <p>connexion</p>
+            </div>
+        </div>
     </div>
-  </div>
+</div>
+
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid  navbarNav p-2">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse  d-flex justify-content-center" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link ps-5" href="#">ACTUALITÉS</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link ps-5" href="#">POLITIQUE</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link ps-5" href="#">ÉCONOMIE</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link ps-5" href="#">SPORT</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link ps-5" href="#">TECHNOLOGIES</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link ps-5" href="#">CULTURE</a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </nav>
