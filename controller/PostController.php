0d7e14e3fddad34a4a5f854545d55b3a1b40f59c
aliciaheraud85 <167339080+aliciaheraud85@users.noreply.github.com>
2024-06-26 12:07:53 +0200

    mise en place de mon controller

--- controller/PostController.php
+++ controller/PostController.php
@@ -1,1 +1,31 @@
- 
+<?php
+
+//1. APPEL DE L'AUTOLOADER
+
+require_once(__DIR__. '/../config/autload.php');
+
+//2.DECLARATION DE MA CLASSE
+
+    class PostController {
+        private $postModel;
+        private $connexion;
+
+        // Mise en place de mon constructeur
+
+        public function __construct($PostModel, $connexion){
+            $this -> postModel = $postModel;
+            $this-> connexion = $connexion;
+        }
+
+        // Déclaration de ma méthode pr mon index
+        public function index(){
+            try{
+                
+
+            }catch(PDOException $e){
+                echo "ERREUR" . $e->getMessage();
+            }
+        }
+    }
+
+?>
