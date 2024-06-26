cfcf21acbae8857a530fb62398138c7e184e1fd6
aliciaheraud85 <167339080+aliciaheraud85@users.noreply.github.com>
2024-06-26 11:51:07 +0200

    autoload

---
+++ config/autoload.php
@@ -0,0 +1,15 @@
+<?php 
+
+spl_autoload_register(function ($class_name){
+    $paths = [
+        __DIR__ . '/../model/',
+        __DIR__ . '/../controller'
+    ];
+    foreach($paths as $path){
+        $file = $path . $class_name . '.php';
+        if(file_exists($file)) {
+            require_once $file;
+        }
+    }
+});
+?>
