e169ff5a98adf5e40e66dcb59037253ec1e8103c
aliciaheraud85 <167339080+aliciaheraud85@users.noreply.github.com>
2024-06-26 14:29:51 +0200

    init postModel

--- model/PostModel.php
+++ model/PostModel.php
@@ -1,1 +1,21 @@
- 
+<?php 
+
+    //1. APPEL DE LA CONNEXION ET DE L'AUTOLOADER
+
+    require_once(__DIR__ . '/../config/autoloader.php');
+    require_once(__DIR__ . '/../config/connexion.php');
+
+    //2. DECLARATION DE MA CLASSE
+
+    class PostModel {
+
+        private $connexion;
+
+        //2.1 constructeur
+        public function __construct($connexion){
+            $this->connexion = $connexion;
+        }
+
+        
+    }
+?>
