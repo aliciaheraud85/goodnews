24e596e3fda5a01dbc5425bc93ee678c02e9e2fd
aliciaheraud85 <167339080+aliciaheraud85@users.noreply.github.com>
2024-06-26 11:48:11 +0200

    connexion db

---
+++ config/connexion.php
@@ -0,0 +1,18 @@
+<?php 
+
+//1. APPEL DE CONFIG POUR LES VARIABLES MEMBRES
+
+    require_once("config.php");
+
+//2. FAIRE LE TRY CATCH
+
+    try{
+        $connexion = new PDO("mysql:localhost=$host;goodnews=$dbname;utf8mb4=$charset", $user, $password);
+        echo "Connexion à la base de donnée réussie.";
+    }catch(PDOException $e){
+        throw new PDOException($e->getMessage(), (int)$e->getCode());
+    }
+
+?>
+
+
