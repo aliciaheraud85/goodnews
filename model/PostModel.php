5e411cf1cfecacc4ddde4c276353415e294ee5f5
aliciaheraud85 <167339080+aliciaheraud85@users.noreply.github.com>
2024-06-27 16:12:33 +0200

    method getonepost

--- model/PostModel.php
+++ model/PostModel.php
@@ -24,5 +24,12 @@
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
         
+        public function getOnePost($id){
+            $query = "SELECT * FROM post WHERE id= :id";
+            $stmt = $this->connexion->prepare($query);
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
     }
 ?>
