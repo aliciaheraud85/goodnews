156746b83d4a96b5c8117de4d403ab06a21291c9
aliciaheraud85 <167339080+aliciaheraud85@users.noreply.github.com>
2024-06-28 09:52:01 +0200

    css readone

--- views/readOne.php
+++ views/readOne.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./divers/css/styles.css">
+    <link rel="stylesheet" href="./divers/css/show.css">
     <title><?php echo htmlspecialchars($onePost['title']); ?></title>
 </head>
 <body>
@@ -19,14 +19,14 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-2"></div>
-                <div class="col-md-8">
+                <div class="col-md-8 article">
                     <?php if(!empty($onePost)): ?>
                         <div class="row">
                             <div class="col-md-12">
-                                <h2><?php echo htmlspecialchars($onePost['title']) ;?></h2>
+                                <h2 class="zxtitre"><?php echo htmlspecialchars($onePost['title']) ;?></h2>
                             </div>
                             <div class="col-md-12">
-                                <h4><?php echo htmlspecialchars($onePost['abstract']) ;?></h4>
+                                <h4 class="zxtitre2"><?php echo htmlspecialchars($onePost['abstract']) ;?></h4>
                             </div>
                             <div class="col-md-12">
                                 <p>Par <?php echo htmlspecialchars($onePost['author']); ?></p>
@@ -35,18 +35,26 @@
                                 <img class="img-fluid" src="./divers/images/<?php echo htmlspecialchars($onePost['image']) ;?>" alt="image du post">
                             </div>
                             <div class="col-md-12">
-                                <p><?php echo htmlspecialchars($onePost['content']); ?></p>
+                                <p class="content"><?php echo htmlspecialchars($onePost['content']); ?></p>
                             </div>
                             <?php if(!empty($onePost) && isset($onePost['content2'])) : ?>
                                 <div class="col-md-12">
-                                    <p><?php echo htmlspecialchars($onePost['content2']); ?></p>
+                                    <p class="content"><?php echo htmlspecialchars($onePost['content2']); ?></p>
                                 </div>
                             <?php endif; ?>
                             <div class="col-md-12">
                                 <p>Publié le : <?php echo htmlspecialchars($onePost['createdAt']); ?></p>
                             </div>
-
-                            
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-md-10">
+                                        <p class="social">Partager sur :</p>
+                                    </div>
+                                    <div class="col-md-2 mb-5">
+                                        <a href="https://www.facebook.com/login.php?skip_api_login=1&api_key=966242223397117&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fsharer%2Fsharer.php%3Fu%3Dhttps%253A%252F%252Fwww.legorafi.fr%252F2024%252F06%252F11%252Fil-sest-converti-en-fichier-pdf-par-erreur%252F&cancel_url=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Fclose_window%2F%3Fapp_id%3D966242223397117%26connect%3D0%23_%3D_&display=popup&locale=fr_FR" target="blank"><i class="fa-brands fa-facebook fa-2x"></i></a><a href="https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww.linkedin.com%2FshareArticle%2F%3Furl%3Dhttps%253A%252F%252Fwww.legorafi.fr%252F2024%252F06%252F11%252Fil-sest-converti-en-fichier-pdf-par-erreur%252F" target="blank"><i class="fa-brands fa-linkedin fa-2x ps-2"></i></a><a href="https://x.com/intent/post?text=Il+s%E2%80%99est+converti+en+fichier+PDF+par+erreur&url=https%3A%2F%2Fwww.legorafi.fr%2F2024%2F06%2F11%2Fil-sest-converti-en-fichier-pdf-par-erreur%2F" target="blank" ><i class="fa-brands fa-twitter fa-2x ps-2"></i></a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     <?php else : ?>
                         <p>Aucun post disponible</p>
