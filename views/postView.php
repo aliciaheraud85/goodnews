326104b40e774cf2d097c29e85c72955d048ae23
aliciaheraud85 <167339080+aliciaheraud85@users.noreply.github.com>
2024-06-29 16:00:42 +0200

    mise en page rubrikCultur

--- views/postView.php
+++ views/postView.php
@@ -146,6 +146,28 @@
                         <p>Aucuns posts disponibles</p>
                     <?php endif ; ?>
 
+                    <!--RUBRIQUE CULTURE -->
+
+                    <?php if(!empty($rubrikCultur)) : ?>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3 class="zxtitre2 mt-3">Culture</h3>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <?php foreach ($rubrikCultur as $cult) : ?>
+                                        <div class="col-md-4">
+                                            <a class="lien" href="index.php?action=show&id=<?php echo htmlspecialchars($cult['id']); ?>"><?php echo htmlspecialchars($cult['title']);?></a>
+                                            <a href="index.php?action=show&id=<?php echo htmlspecialchars($cult['id']); ?>"><img class="img-fluid align-self-center img_lien" src="./divers/images/<?php echo htmlspecialchars($cult['image']); ?>" alt="image du post"></a>
+                                        </div>
+                                    <?php endforeach; ?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php else : ?>
+                        <p>Aucuns posts disponibles</p>
+                    <?php endif ; ?>
+
 
             </div>
             <div class="col-md-1"></div>
