3076bd347c48f90caa34045c631bae4e6e720fbe
aliciaheraud85 <167339080+aliciaheraud85@users.noreply.github.com>
2024-06-29 15:56:46 +0200

    var rubrikTech

--- controller/PostController.php
+++ controller/PostController.php
@@ -25,6 +25,8 @@ require_once(__DIR__. '/../config/autoload.php');
                 $rubrikPolitik = $this->postModel->getRubrikPolitik(3);
                 $rubrikEco = $this->postModel->getRubrikEco(3);
                 $rubrikSport = $this->postModel->getRubrikSport(3);
+                $rubrikTech = $this->postModel->getRubrikTech(3);
+
                 include(__DIR__ . '/../views/postView.php');
             }catch(PDOException $e){
                 echo "ERREUR" . $e->getMessage();
