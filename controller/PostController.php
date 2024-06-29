f3d6e1b4f22e395a618fa8b547eab2662fdef4f8
aliciaheraud85 <167339080+aliciaheraud85@users.noreply.github.com>
2024-06-29 15:18:12 +0200

    rubrik politik et eco

--- controller/PostController.php
+++ controller/PostController.php
@@ -22,6 +22,8 @@ require_once(__DIR__. '/../config/autoload.php');
             try{
                 $newPost = $this->postModel->getNewPost(9);
                 $rubrikActu = $this->postModel->getRubrikActu(3);
+                $rubrikPolitik = $this->postModel->getRubrikPolitik(3);
+                $rubrikEco = $this->postModel->getRubrikEco(3);
                 include(__DIR__ . '/../views/postView.php');
             }catch(PDOException $e){
                 echo "ERREUR" . $e->getMessage();
