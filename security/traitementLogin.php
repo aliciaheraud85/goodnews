dfe54aae00cdf5cc073d5a924883cb4463cef45f
aliciaheraud85 <167339080+aliciaheraud85@users.noreply.github.com>
2024-07-01 09:58:30 +0200

    traitementRegister

--- security/traitementLogin.php
+++ security/traitementLogin.php
@@ -1,1 +1,7 @@
- 
+ <?php 
+//1. APPEL A LA CONNEXION
+
+ require_once(__DIR__ . '/../config/connexion.php');
+
+ /2
+ ?>
--- security/traitementRegister.php
+++ security/traitementRegister.php
@@ -1,1 +1,44 @@
- 
+<?php
+
+//1. APPEL A LA CONNEXION
+
+    require_once(__DIR__ . '/../config/connexion.php');
+
+//2. VERIFICATION DE LA SOUMISSION DU FORMULAIRE
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){ //s'assure de quelle methode nous avons utilisé dans le formulaire (GET OU POST)
+
+        $rgpd = isset($_POST['rgpd']) ? 1 : 0; //verifie que la case rgpd est bien coché
+
+        //2.1 ON RECUPERE LES DONNEES DU FORMULAIRE
+        $user_name = trim($_POST['user_name']);
+        $lastname = strtroupper(trim($_POST['lastname'])); //strtroupper = tout en MAJ
+        $firstname = ucfirst(strtolower(trim($_POST['firstname']))); // ucfirst = 1erelettre en MAJ / strtolower = autre lettres en MIN / trim = nettoie pr éviter tt caractère indésirable (protège requêtr SQL)
+        $email = trim($_POST['email']);
+        
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT); //Hacher le mot de passe
+
+        $city = ucfirst(strtolower(trim($_POST['city'])));
+        $country = trim($_POST['country']);
+
+        $role = 'user'; //on définit le rôle par défaut
+
+        //2.2 REQUETE SQL
+
+        try{
+            $sql = "INSERT INTO users(user_name, lastname, firstname, email, password, city, country, rgpd, role)
+                    VALUES(:user_name, :lastname, :firstname, :email, :password, :city, :country, :rgpd, :role)";
+
+            $stmt = $connexion->prepare($sql);
+
+            $stmt->execute([$user_name, $lastname, $firstname, $email, $password, $city, $country, $rgpd, $role]);
+
+            echo "Inscription réussie";
+        }catch(PDOException $e){
+            echo "ERREUR" . $e->getMessage();
+        }
+
+
+    }
+
+?>
